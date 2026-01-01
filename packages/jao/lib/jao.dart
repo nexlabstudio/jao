55b2f6221712420befd01a81670d9a3954103676
Codefarmer <abadasamuelosp@gmail.com>
2026-01-01 12:03:01 +0100

    chore: expose model metadata and registry

--- packages/jao/lib/jao.dart
+++ packages/jao/lib/jao.dart
@@ -19,3 +19,5 @@ export 'src/migrations/migration.dart';
 export 'src/migrations/generator.dart';
 export 'src/jao.dart';
 export 'src/utils/type_converters.dart';
+export 'src/meta/model_meta.dart';
+export 'src/meta/model_registry.dart';
